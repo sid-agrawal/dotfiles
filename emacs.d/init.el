a287af32d824c52f68feb1a5db190d9adc8380ba
Sid Agrawal <agrawal.bitsg@gmail.com>
2021-01-03 16:04:38 -0800

    Add python section for dap and lsp

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -424,8 +424,19 @@
   (exec-path-from-shell-initialize))
 
 (use-package python-mode
-  :mode "\\.py\\'"
-  :hook (python-mode . lsp-deferred))
+  :ensure t
+  :hook (python-mode . lsp-deferred)
+  :custom
+  ;; NOTE: Set these if Python 3 is called "python3" on your system!
+  ;; (python-shell-interpreter "python3")
+  ;; (dap-python-executable "python3")
+  (dap-python-debugger 'debugpy)
+  :config
+  (require 'dap-python))
+
+(use-package pyvenv
+  :config
+  (pyvenv-mode 1))
 
 (use-package company
   :after lsp-mode
